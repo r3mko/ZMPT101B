5e1abf1accf6120646bbe91fe0fd6dc74d2f4dc6
Remko Kleinjan <r.a.kleinjan@gmail.com>
2023-01-26 16:38:43 +0100

    Cleanup

--- src/ZMPT101B.cpp
+++ src/ZMPT101B.cpp
@@ -7,10 +7,12 @@ ZMPT101B::ZMPT101B(uint8_t _pin) {
 
 int ZMPT101B::calibrate() {
 	uint16_t acc = 0;
+
 	for (int i = 0; i < 10; i++) {
 		acc += analogRead(pin);
 	}
 	zero = acc / 10;
+
 	return zero;
 }
 
@@ -28,10 +30,12 @@ void ZMPT101B::setSensitivity(float sens) {
 
 float ZMPT101B::getVoltageDC() {
 	int16_t acc = 0;
+
 	for (int i = 0; i < 10; i++) {
 		acc += analogRead(pin) - zero;
 	}
 	float V = (float)acc / 10.0 / ADC_SCALE * Vref / sensitivity;
+
 	return V;
 }
 
@@ -39,15 +43,16 @@ float ZMPT101B::getVoltageAC(uint16_t frequency) {
 	uint32_t period = 1000000 / frequency;
 	uint32_t t_start = micros();
 
-	uint32_t Vsum = 0, measurements_count = 0;
+	uint32_t Vsum = 0;
+	uint32_t measurements_count = 0;
 	int32_t Vnow;
 
 	while (micros() - t_start < period) {
 		Vnow = analogRead(pin) - zero;
-		Vsum += Vnow*Vnow;
+		Vsum += Vnow * Vnow;
 		measurements_count++;
 	}
-
 	float Vrms = sqrt(Vsum / measurements_count) / ADC_SCALE * Vref / sensitivity;
+
 	return Vrms;
 }
